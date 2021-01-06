3f84440c40e728faf1a3d7b0cd5e4e326fda81f2
MarceloFRSilva <marregsil97@gmail.com>
2021-01-06 11:43:30 +0100

    Improving the code a little

--- PicoCTF/Tapping/morseCodeTool.py
+++ PicoCTF/Tapping/morseCodeTool.py
@@ -36,17 +36,43 @@ morse_code_alphabet = {".-" : 'A',
                       "----." : '9',
                       "-----" : '0',}
 
-morse_code_message = ".--. .. -.-. --- -.-. - ..-. { -- ----- .-. ... ...-- -.-. ----- -.. ...-- .---- ... ..-. ..- -. .---- ..--- -.... .---- ....- ...-- ---.. .---- ---.. .---- }"
+def encode():
+    morse_code_message = input("Message to encode: ")
+    for key,value in morse_code_alphabet.items():
+        for i in morse_code_message:
+            if(value == i):
+                print(key + " ", end='')
+    print()
 
-message_chars_list = morse_code_message.split( )
 
-size = len(morse_code_alphabet)
+def decode():
+    morse_code_message = input("Message to decode: ")
+    message_chars_list = morse_code_message.split( )
+    for i in message_chars_list:
+        if(i == "{" or i == "}"):
+            print(i, end='')
+        else:    
+            for key in morse_code_alphabet:
+                if(i == key):
+                    print(morse_code_alphabet[key], end='')
+    print()
 
-for i in message_chars_list:
-    if(i == "{" or i == "}"):
-        print(i, end='')
-    else:    
-        for key in morse_code_alphabet:
-            if(i == key):
-                print(morse_code_alphabet[key], end='')
-print()
+
+
+
+def main():
+    print("Welcome to the morse code tool")
+    print("Menu:")
+    print("1 - Encode")
+    print("2 - Decode")
+    option = input("Choose one option: ")
+    if(option == str(1)):
+        encode()
+    elif(option == str(2)):
+        decode()
+    else:
+        print("Wrong option, try again.")
+        main()
+
+if __name__ == "__main__":
+    main()

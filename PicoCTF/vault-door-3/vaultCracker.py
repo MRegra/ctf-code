43189eaec454a8f30091823b6dc46822a66103fe
MarceloFRSilva <marregsil97@gmail.com>
2021-01-20 18:10:56 +0100

    Script for the vault-door-3 challenge added

---
+++ PicoCTF/vault-door-3/vaultCracker.py
@@ -0,0 +1,21 @@
+
+
+def checkPassword(password):
+    if (not len(password) == 32):
+        return False
+    buffer = [""]*32
+    print(buffer)
+    for i in range(8):
+        buffer[i] = password[i]
+    for i in range(8, 16):
+        buffer[i] = password[23-i]
+    for i in range(16, 32, 2):
+        buffer[i] = password[46-i]
+    for i in range(31, 16, -2):
+        buffer[i] = password[i] 
+    print(''.join(buffer))
+
+checkPassword("jU5t_a_sna_3lpm12g94c_u_4_m7ra41")    
+
+#picoCTF{jU5t_a_s1mpl3_an4gr4m_4_u_c79a21}
+

